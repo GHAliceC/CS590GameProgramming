920de8a80a7316cccedb1967ceeb17b6c8085630
Yueying Liu <648339108@qq.com>
2020-04-28 01:19:25 -0400

    can switch between 1st and 3rd person

---
+++ Liu.Yueying.Chen.Yuwei.FinalProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject thirdCam;
+	public GameObject firstCam;
+	public int camMode;  // 1 as first person, 3 as third person
+
+	bool enterMouse;
+
+	// Use this for initialization
+	void Start () {
+		camMode = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log (camMode);
+		enterMouse = player.GetComponent<PlayerController> ().enterMouse;
+//		Debug.Log (enterMouse);
+		if (Input.GetButtonDown ("Out")) {
+			if (camMode == 3) {
+				camMode = 1;
+				Debug.Log (camMode);
+				Debug.Log ("switch to first");
+				firstCam.SetActive (true); 
+				thirdCam.SetActive (false);
+				player.GetComponent<PlayerController> ().enterMouse = false;
+
+			}
+		}
+		else if (enterMouse) {
+//			Debug.Log (camMode);
+			if (camMode == 1) {
+				camMode = 3;
+				Debug.Log ("switch to third");
+				thirdCam.SetActive (true);
+				firstCam.SetActive (false); 
+			}
+		}
+	}
+		
+}
---
+++ Liu.Yueying.Chen.Yuwei.FinalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// This script should be attached to the mouse that the player controls
+	// Also, add CharacterController component on the mouse
+
+	public bool enterMouse;
+
+	float moveSpeed;
+	float turnSpeed;
+	CharacterController controller;
+	Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		moveSpeed = 200f;
+		turnSpeed = 100f;
+		enterMouse = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// move forward
+		Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+		move = this.transform.TransformDirection(move);
+		controller.Move(move * moveSpeed);
+		// turn left and right
+		rotation = new Vector3 (0, Input.GetAxisRaw ("Horizontal") * turnSpeed * Time.deltaTime, 0);
+		this.transform.Rotate(rotation);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "mouse")
+		{
+//			Debug.Log ("enter mouse");
+			enterMouse = true;
+		}
+	}
+		
+}
