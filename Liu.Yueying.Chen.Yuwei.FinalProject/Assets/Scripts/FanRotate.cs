64628c61533f31201dbc9937f2c1079a9b3f87f3
Alice Chen <31550011+GHAliceC@users.noreply.github.com>
2020-05-03 18:03:36 -0400

    setup windmill and seesaw

--- Liu.Yueying.Chen.Yuwei.FinalProject/Assets/Scripts/FanRotate.cs
+++ Liu.Yueying.Chen.Yuwei.FinalProject/Assets/Scripts/FanRotate.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class FanRotate : MonoBehaviour {
 
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate (new Vector3 (0f, 0f, 1f));
-	}
+	void Update() {
+//		transform.Rotate(Vector3.down * Time.deltaTime * 100);
+//		if (Input.GetKeyDown ("space")) {
+		transform.Rotate (0, 0, 1f);
+		if (transform.localRotation.z <= -40) {
+			transform.Rotate (0, 0, 1.0f * Time.deltaTime * 100);
+		}
 
+		if (transform.localRotation.z >= 40) {
+			transform.Rotate (0, 0, -1.0f * Time.deltaTime * 100);
+		}
+//		}
+	}
 
 }
---
+++ Liu.Yueying.Chen.Yuwei.FinalProject/Assets/Scripts/SeesawRotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeesawRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")) {
+			transform.Rotate(0, 0, 1f);
+			if (transform.localRotation.z <= -10)
+			{
+				transform.Rotate(0, 0, 1.0f * Time.deltaTime * 100 );
+			}
+
+			if (transform.localRotation.z >= 10)
+			{
+				transform.Rotate(0, 0, -1.0f * Time.deltaTime * 100 );
+			}
+		}
+//		transform.Rotate(0, 0, 1f);
+//		if (transform.localRotation.z <= -10)
+//		{
+//			transform.Rotate(0, 0, 1.0f * Time.deltaTime * 100);
+//		}
+//
+//		if (transform.localRotation.z >= 10)
+//		{
+//			transform.Rotate(0, 0, -1.0f * Time.deltaTime * 100);
+//		}
+	}
+}
